2fd25f5c297c7d641c902e8ce33927f52be8e02f
henrique26 <fabricioohh@gmail.com>
2024-05-30 13:45:52 -0300

    refactor: adjusted string button and image screen

--- src/components/ModalFilter.tsx
+++ src/components/ModalFilter.tsx
@@ -43,7 +43,7 @@ export default function ModalFilter({ setVisible }: Props) {
               style={styles.icon}
             />
             <Text style={styles.titleSelect}>
-              Male
+              Female
             </Text>
           </TouchableOpacity>
 
